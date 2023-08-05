baa43453da20c88874a781069731307601b770af
muhammad abdul ahad <abdulahadmailacc@gmail.com>
2023-08-05 20:06:49 +0600

    create seed service

---
+++ services/seed.service.js
@@ -0,0 +1,12 @@
+const data = require("../data");
+const User = require("../models/User");
+
+//seed service for deleteMany and insertMany
+const  userSeed = async()=>{
+    //before delete all documents from user collection.
+    await User.deleteMany({})
+    
+    //Insert many documents in user collection.
+    const result = await User.insertMany(data.users)
+    return result
+}
